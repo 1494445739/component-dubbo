bfc68fd8f81ca709af789ee161c32e5e6057b58b
zenglin <zengl@tzg.cn>
2017-08-24 10:42:19 +0800

    add dubbo async callback illustraction

---
+++ src/main/java/com.tzg.dubbo.listener/NotifyImpl.java
@@ -0,0 +1,13 @@
+package com.tzg.dubbo.listener;
+
+public class NotifyImpl {
+
+//    public void onreturn( T obj ) {
+//        System.out.println( "onreturn callback. The return value is: " + obj );
+//    }
+//
+//    public void onthrow( Throwable ex ) {
+//        System.out.println( "onthrow callback. The exception is: " + ex.getMessage() );
+//    }
+
+}
